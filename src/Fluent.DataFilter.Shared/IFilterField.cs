f3d669ebce18d9b866f557085618dfa3d6367958
hehai <gzhehai@foxmail.com>
2017-06-05 17:17:38 +0800

    Updated add remarks

--- src/Fluent.DataFilter.Shared/IFilterField.cs
+++ src/Fluent.DataFilter.Shared/IFilterField.cs
@@ -6,7 +6,15 @@ namespace Fluent.DataFilter
     public interface IFilterField
     {
         bool IsSatisfy();
+
+        /// <summary>
+        /// Get an type of field
+        /// </summary>
         Type FieldType { get; }
+
+        /// <summary>
+        /// Get an type of filter field element
+        /// </summary>
         Type FilterFieldType { get; }
     }
 }
