cfa39ea9bf49a8f5228bead915978b4132274699
hehai <gzhehai@foxmail.com>
2017-06-25 23:59:09 +0800

    Constraint TEntity where: class Limit

--- src/FluentFilter/DataFilter`.cs
+++ src/FluentFilter/DataFilter`.cs
@@ -4,6 +4,7 @@
     [Serializable]
 #endif
     public abstract class DataFilter<TEntity, TFilterEntity> : IDataFilter<TFilterEntity>
+        where TEntity : class
         where TFilterEntity : IDataFilter<TFilterEntity>
     {
     }
