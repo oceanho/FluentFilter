cf3725fd5d8db675a2c28a2cd936f6551e97ecd3
hehai <gzhehai@foxmail.com>
2017-07-06 01:21:37 +0800

    Add ExprTreeOptimizer

---
+++ src/FluentFilter/Inetnal/ExprTreeVisitors/Modifiers/ExprTreeOptimizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+
+namespace FluentFilter.Inetnal.ExprTreeVisitors.Modifiers
+{
+    internal class ExprTreeOptimizer : ExpressionVisitor
+    {
+        private Expression m_rawExpr;
+        public ExprTreeOptimizer(Expression expr)
+        {
+            m_rawExpr = expr;
+        }
+
+        public Expression Optimize()
+        {
+            return Visit(m_rawExpr);
+        }
+
+        protected override Expression VisitBlock(BlockExpression node)
+        {
+            // if(node.)
+            return base.VisitBlock(node);
+        }
+    }
+}
